7017719d049194c2a86b18243e2169b16e54a116
Adriano Faria Alves <adriano.faria@outlook.com.br>
2018-09-07 12:36:54 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/siswbrasil/workshopmongo/config/Instantiation.java
+++ src/main/java/com/siswbrasil/workshopmongo/config/Instantiation.java
@@ -1,30 +1,46 @@
 package com.siswbrasil.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+
+import com.siswbrasil.workshopmongo.domain.Post;
 import com.siswbrasil.workshopmongo.domain.User;
+import com.siswbrasil.workshopmongo.repository.PostRepository;
 import com.siswbrasil.workshopmongo.repository.UserRepository;
 
 @Configuration
-public class Instantiation implements CommandLineRunner{
-	
+public class Instantiation implements CommandLineRunner {
+
 	private final UserRepository userRepository;
-	
-	public Instantiation(UserRepository userRepository) {
+	private final PostRepository postRepository;
+
+	public Instantiation(UserRepository userRepository, PostRepository postRepository) {
 		super();
 		this.userRepository = userRepository;
+		this.postRepository = postRepository;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
 		userRepository.deleteAll();
+		postRepository.deleteAll();
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo, Abraços!",maria);
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
